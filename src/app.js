8db3848ab96f586afa2bad5448485a4398250f31
Henry Gordon <hwgordon247@gmail.com>
2017-05-31 21:07:32 +0100

    basic implementation of mongoose with db integration

--- src/app.js
+++ src/app.js
@@ -1,54 +1,36 @@
 const express = require('express')
 const app = express()
 
-var MongoClient = require('mongodb').MongoClient
+// var MongoClient = require('mongodb').MongoClient
+let mongoose = require('mongoose');
+let User = require('./models/user');
 
 // Connection URL
-// var url = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@localhost:27017/fairgame`;
-var url = `mongodb://localhost:27017/fairgame`;
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  console.log("Connected successfully to server");
-
-  insertDocuments(db, function() {
-    findDocuments(db, function() {
-      db.close();
-    });
-  });
-});
+// let url = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@localhost:27017/fairgame`;
+let url = 'mongodb://localhost:27017/fairgame';
+mongoose.Promise = global.Promise;
+mongoose.connect(url);
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 app.get('/store', function (req, res) {
-  res.send('yo')
+  let chris = new User(req.query);
+
+  chris.save(err => {
+    if (err) console.log(err)
+    res.send('yo')
+  })
+})
+
+app.get('/list', (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) console.log(err)
+    res.send(users)
+  })
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
-
-var insertDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Insert some documents
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-}
-
-var findDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Find some documents
-  collection.find({}).toArray(function(err, docs) {
-    console.log("Found the following records");
-    console.log(docs)
-    callback(docs);
-  });
-}
