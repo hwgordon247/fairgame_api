86165abae6cd239a593fa8aeaeb3ec0ff9550fc7
Henry Gordon <hwgordon247@gmail.com>
2017-06-02 14:42:18 +0100

    creation of config

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Routes = require('./src/routes/routes');
+const config = require('config');
 
 new Routes(app);
 
 // Connection URL
 // let url = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@localhost:27017/fairgame`;
-const url = 'mongodb://localhost:27017/fairgame';
 mongoose.Promise = global.Promise;
-mongoose.connect(url);
+mongoose.connect(config.databaseUrl);
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
