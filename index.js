25afbb5142334040c25ac0d892d396fb6a139c6b
Henry Gordon <hwgordon247@gmail.com>
2017-07-09 17:51:44 +0100

    addition of CORS middleware

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+app.use((req, res, next) => {
+  console.log('herer');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // const exampleMiddleware = new ExampleMiddleware();
 // app.use((req, res, next) => { exampleMiddleware.run(req, res, next, 'howdy'); });
 
